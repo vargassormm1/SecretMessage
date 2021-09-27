fd230a03b5f73eab627f5dc19b2032d56286431d
mattvargas <vargassormm1@montclair.edu>
2021-09-27 12:06:12 -0400

    add bootstrap and changed up styling

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const { hash } = window.location;
 const secretMessage = atob(hash.replace("#", ""));
 
 if (secretMessage) {
-  document.querySelector("#message-form").classList.add("hide");
-  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("#message-form").classList.add("d-none");
+  document.querySelector("#message-show").classList.remove("d-none");
 
-  document.querySelector("h1").innerHTML = secretMessage;
+  document.querySelector("#message").innerHTML = secretMessage;
 }
 
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  document.querySelector("#message-form").classList.add("hide");
-  document.querySelector("#link-form").classList.remove("hide");
+  document.querySelector("#message-form").classList.add("d-none");
+  document.querySelector("#link-form").classList.remove("d-none");
 
   const input = document.querySelector("#message-input");
   const encrypted = btoa(input.value);
